6a5f7c8c6ebd40485e2f106ab20b7d4d850e7ae5
zaidoh2015 <danfealmeida@example.com>
2018-09-05 13:50:02 -0300

    config2 heroku

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 #
