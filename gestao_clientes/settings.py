d39b392aaa1c72b21da268e90dfbf7b278031bac
zaidoh2015 <danfealmeida@example.com>
2018-09-05 14:26:22 -0300

    Configuring the app

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes-dev.herokuapp.com']
+ALLOWED_HOSTS = ['gestao-clientes-dev.herokuapp.com/']
 
 # Application definition
 
@@ -146,3 +146,4 @@ LOGIN_REDIRECT_URL = 'persons_list'
 LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
